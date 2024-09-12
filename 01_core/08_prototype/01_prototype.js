44624889836ae18bfbdb6f4fb9aaa469ce343413
kimsua <k021113a@naver.com>
2024-09-12 15:56:54 +0900

    *

--- 01_core/08_prototype/01_prototype.js
+++ 01_core/08_prototype/01_prototype.js
@@ -1,8 +1,19 @@
 // 프로토타입
 
 /* 
-모든 객체는 자산의 부모 객체로 연결되는
+모든 객체는 자산의 부모 객체로 연결되는 프로퍼티인 [prototype] 을 가지고 있다.
+프로퍼티인 [prototype]을 가지고 있다.
+이 때 상속으 받는 부모 객체를 프로토타입 이라고 부른다.
+객체가 특정 프로퍼티나 메소드를 찾을 때 먼저 그 객체에서 찾고
+없으면 객체의 프로토타입에서 찾는다.
+프로토타입이 또 다른 프로토타입을 가지며 부모 객체를 따라 올라가고 
+프로토타입 체인이 형성된다.
 */
+
+const obj = {};
+console.log(obj.toString());
+
+console.log(Object.prototype.hasOwnProperty("to?String"));
 
 const user2= {
     greet(){
